90a112003fddaa66dc1249f7afc9f505eef9a532
lidiettes <hello@lidiettes.com>
2023-03-25 18:13:23 +0100

    I comment on the tests to improve readability

--- src/utils/mower.test.js
+++ src/utils/mower.test.js
@@ -1,43 +1,52 @@
 const { moveMower } = require('./mower');
 
-test('the moveMower function is executed without errors', () => {
-    const mower = { x: 0, y: 0, orientation: 'N' };
-    const instructions = ['M', 'R', 'M'];
+describe("Given a demo test", () => {
+    describe("When test are executed", () => {
+        describe("And I want test movements", () => {
+            test('the moveMower function is executed without errors', () => {
+                const mower = { x: 0, y: 0, orientation: 'N' };
+                const instructions = ['M', 'R', 'M'];
 
-    expect(() => moveMower(mower, instructions)).not.toThrow();
-});
+                expect(() => moveMower(mower, instructions)).not.toThrow();
+            });
 
-test('moves correctly to the north', () => {
-    const mower = { x: 0, y: 0, orientation: 'N' };
-    const instructions = ['M', 'M', 'M'];
+            test('moves correctly to the north', () => {
+                const mower = { x: 0, y: 0, orientation: 'N' };
+                const instructions = ['M', 'M', 'M'];
 
-    expect(moveMower(mower, instructions)).toBe('0 3 N');
-});
+                expect(moveMower(mower, instructions)).toBe('0 3 N');
+            });
 
-test('moves correctly to the east', () => {
-    const mower = { x: 0, y: 0, orientation: 'E' };
-    const instructions = ['M'];
+            test('moves correctly to the east', () => {
+                const mower = { x: 0, y: 0, orientation: 'E' };
+                const instructions = ['M'];
 
-    expect(moveMower(mower, instructions)).toBe('1 0 E');
-});
+                expect(moveMower(mower, instructions)).toBe('1 0 E');
+            });
 
-test('moves correctly to the south ', () => {
-    const mower = { x: 1, y: 1, orientation: 'S' };
-    const instructions = ['M'];
+            test('moves correctly to the south ', () => {
+                const mower = { x: 1, y: 1, orientation: 'S' };
+                const instructions = ['M'];
 
-    expect(moveMower(mower, instructions)).toBe('1 0 S');
-});
+                expect(moveMower(mower, instructions)).toBe('1 0 S');
+            });
 
-test('moves correctly to the west', () => {
-    const mower = { x: 1, y: 1, orientation: 'W' };
-    const instructions = ['M'];
+            test('moves correctly to the west', () => {
+                const mower = { x: 1, y: 1, orientation: 'W' };
+                const instructions = ['M'];
 
-    expect(moveMower(mower, instructions)).toBe('0 1 W');
-});
+                expect(moveMower(mower, instructions)).toBe('0 1 W');
+            });
+        });
 
-test('doesnt move out of the area', () => {
-    const mower = { x: 0, y: 0, orientation: 'S' };
-    const instructions = ['M', 'M', 'M'];
+        describe("And I want test the are", () => {
+            test('doesnt move out of the area', () => {
+                const mower = { x: 0, y: 0, orientation: 'S' };
+                const instructions = ['M', 'M', 'M'];
 
-    expect(() => moveMower(mower, instructions)).toThrow(/Mower out of area/);
-});
+                expect(() => moveMower(mower, instructions)).toThrow(/Mower out of area/);
+            });
+        })
+
+    })
+})
