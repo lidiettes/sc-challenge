e6fe0dcd01982048f69db7ff8c095e2a47d30518
lidiettes <hello@lidiettes.com>
2023-03-25 18:41:30 +0100

    spelling mistakes

--- src/utils/mower.test.js
+++ src/utils/mower.test.js
@@ -6,7 +6,6 @@ describe("Given a demo test", () => {
             test('the moveMower function is executed without errors', () => {
                 const mower = { x: 0, y: 0, orientation: 'N' };
                 const instructions = ['M', 'R', 'L'];
-
                 expect(() => moveMower(mower, instructions)).not.toThrow();
             });
         });
@@ -15,28 +14,24 @@ describe("Given a demo test", () => {
             test('Then should moves correctly to the north', () => {
                 const mower = { x: 0, y: 0, orientation: 'N' };
                 const instructions = ['M', 'M', 'M'];
-
                 expect(moveMower(mower, instructions)).toBe('0 3 N');
             });
 
             test('Then should moves correctly to the east', () => {
                 const mower = { x: 0, y: 0, orientation: 'E' };
                 const instructions = ['M'];
-
                 expect(moveMower(mower, instructions)).toBe('1 0 E');
             });
 
             test('Then should moves correctly to the south ', () => {
                 const mower = { x: 1, y: 1, orientation: 'S' };
                 const instructions = ['M'];
-
                 expect(moveMower(mower, instructions)).toBe('1 0 S');
             });
 
             test('Then should moves correctly to the west', () => {
                 const mower = { x: 1, y: 1, orientation: 'W' };
                 const instructions = ['M'];
-
                 expect(moveMower(mower, instructions)).toBe('0 1 W');
             });
         });
@@ -45,7 +40,6 @@ describe("Given a demo test", () => {
             test('Then shouldnt move out of the area', () => {
                 const mower = { x: 0, y: 0, orientation: 'S' };
                 const instructions = ['M', 'M', 'M'];
-
                 expect(() => moveMower(mower, instructions)).toThrow(/Mower out of area/);
             });
         })
