60f9ff63a4053e433954412c3228ac6fec438516
lidiettes <hello@lidiettes.com>
2023-03-25 18:40:45 +0100

    spelling mistakes

--- src/utils/mower.test.js
+++ src/utils/mower.test.js
@@ -2,36 +2,38 @@ const { moveMower } = require('./mower');
 
 describe("Given a demo test", () => {
     describe("When tests are executed", () => {
-        describe("And I want test movements", () => {
+        describe("When is invoked", () => {
             test('the moveMower function is executed without errors', () => {
                 const mower = { x: 0, y: 0, orientation: 'N' };
-                const instructions = ['M', 'R', 'M'];
+                const instructions = ['M', 'R', 'L'];
 
                 expect(() => moveMower(mower, instructions)).not.toThrow();
             });
+        });
 
-            test('moves correctly to the north', () => {
+        describe("And I want test movements", () => {
+            test('Then should moves correctly to the north', () => {
                 const mower = { x: 0, y: 0, orientation: 'N' };
                 const instructions = ['M', 'M', 'M'];
 
                 expect(moveMower(mower, instructions)).toBe('0 3 N');
             });
 
-            test('moves correctly to the east', () => {
+            test('Then should moves correctly to the east', () => {
                 const mower = { x: 0, y: 0, orientation: 'E' };
                 const instructions = ['M'];
 
                 expect(moveMower(mower, instructions)).toBe('1 0 E');
             });
 
-            test('moves correctly to the south ', () => {
+            test('Then should moves correctly to the south ', () => {
                 const mower = { x: 1, y: 1, orientation: 'S' };
                 const instructions = ['M'];
 
                 expect(moveMower(mower, instructions)).toBe('1 0 S');
             });
 
-            test('moves correctly to the west', () => {
+            test('Then should moves correctly to the west', () => {
                 const mower = { x: 1, y: 1, orientation: 'W' };
                 const instructions = ['M'];
 
@@ -40,7 +42,7 @@ describe("Given a demo test", () => {
         });
 
         describe("And I want test the area", () => {
-            test('doesnt move out of the area', () => {
+            test('Then shouldnt move out of the area', () => {
                 const mower = { x: 0, y: 0, orientation: 'S' };
                 const instructions = ['M', 'M', 'M'];
 
