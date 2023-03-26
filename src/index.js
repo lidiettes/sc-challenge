d509b01fa490ee024db93ed990fe4867d4108588
lidiettes <hello@lidiettes.com>
2023-03-27 01:29:54 +0200

    improving the legibility of naming

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const { moveMower } = require('./utils/mower')
 
 const processArg = [...process.argv]
 // quito las rutas primeras
-const argument = processArg.slice(2, process.argv.length)
+const data = processArg.slice(2, process.argv.length)
+console.log(data)
+
 console.log("App started..");
 
 //cogemos el 55 para saber los limites
-const rightUpFinalCoord = argument.slice(0, 1)
+const rightUpFinalCoord = data.slice(0, 1)
 const maxCoord = {
     x: Number(rightUpFinalCoord[0].substring(0, 1)),
     y: Number(rightUpFinalCoord[0].substring(1, 2))
@@ -16,12 +18,12 @@ console.log(`Upper right point: { x: ${maxCoord.x} y: ${maxCoord.y} }`);
 
 //formateamos instrucciones
 const instructions = []
-for (let i = 1; i < argument.length; i += 2) {
+for (let i = 1; i < data.length; i += 2) {
     const move = {
-        x: Number(argument[i].substring(0, 1)),
-        y: Number(argument[i].substring(1, 2)),
-        orientation: argument[i].substring(2, 3),
-        moves: argument[i + 1]
+        x: Number(data[i].substring(0, 1)),
+        y: Number(data[i].substring(1, 2)),
+        orientation: data[i].substring(2, 3),
+        moves: data[i + 1]
     }
     instructions.push(move)
     console.log(`Parsed instruction are: ${JSON.stringify(move)}`);
