2a7b4d398b8ccbd71b39f138c1c874a78de35887
lidiettes <hello@lidiettes.com>
2023-03-27 01:31:43 +0200

    spaces deleted

--- src/utils/mower.test.js
+++ src/utils/mower.test.js
@@ -3,32 +3,27 @@ const { moveMower } = require('./mower');
 describe("MoveMower unit test", () => {
     const maxCoord = { x: 5, y: 5 };
     test('the moveMower function should run without errors if params are correct', () => {
-
         const instructions = [{ moves: 'MRL', x: 1, y: 2, orientation: 'N' }];
         expect(() => moveMower(maxCoord, instructions)).not.toThrow();
     });
 
     describe("And I want test movements", () => {
         test('The mowerMower function should correctly to the north', () => {
-
             const instructions = [{ moves: 'MMM', x: 0, y: 0, orientation: 'N' }];
             expect(moveMower(maxCoord, instructions)[0]).toBe('03N');
         });
 
         test('Then should moves correctly to the east', () => {
-
             const instructions = [{ moves: 'M', x: 0, y: 0, orientation: 'E' }];
             expect(moveMower(maxCoord, instructions)[0]).toBe('10E');
         });
 
         test('Then should moves correctly to the south ', () => {
-
             const instructions = [{ moves: 'M', x: 0, y: 2, orientation: 'S' }];
             expect(moveMower(maxCoord, instructions)[0]).toBe('01S');
         });
 
         test('Then should moves correctly to the west', () => {
-
             const instructions = [{ moves: 'M', x: 2, y: 0, orientation: 'W' }];
             expect(moveMower(maxCoord, instructions)[0]).toBe('10W');
         });
@@ -36,7 +31,6 @@ describe("MoveMower unit test", () => {
 
     describe("And I want test the area", () => {
         test('Then shouldnt move out of the area', () => {
-
             const instructions = [{ moves: 'M', x: 5, y: 5, orientation: 'N' }];
             expect(() => moveMower(maxCoord, instructions)).toThrow("Mower out of area");
         });
